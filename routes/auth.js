006a82441e6bfc84036238b83c6c7791372b612e
doleksiyenko <dan.oleksiyenko@gmail.com>
2020-07-04 12:04:19 -0400

    logging in

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const Joi = require("@hapi/joi");
 const bcrypt = require("bcrypt");
 
-const userValidation = require("../validation");
 // Models
 const User = require("../models/User");
 const validateUserDetails = require("../validation");
@@ -12,18 +10,17 @@ const validateUserDetails = require("../validation");
 router.post("/register", async (req, res) => {
     // validate
     const validation = validateUserDetails.validate({
-        displayName: req.body.displayName,
-        password: req.body.password,
+        displayName: req.body.displayName.trim(),
+        password: req.body.password.trim(),
     });
     if (validation.error) {
         res.status(400).send(validation.error.details[0].message);
     } else {
         // encrypt the password
-        // const salt = bcrypt.genSalt();
         const hash = bcrypt.hashSync(req.body.password, 10);
         // the validation succeeded, so create the new user
         const newUser = new User({
-            displayName: req.body.displayName,
+            displayName: req.body.displayName.trim(),
             password: hash,
         });
         // try saving the user to the database
@@ -39,8 +36,38 @@ router.post("/register", async (req, res) => {
     }
 });
 
-router.post("/login", (req, res) => {
-    res.send("login");
+router.post("/login", async (req, res) => {
+    // the validation is in both the registration and login in case email is added later for registration.
+    const validation = validateUserDetails.validate({
+        displayName: req.body.displayName.trim(),
+        password: req.body.password.trim(),
+    });
+    // check whether the formatting on the input fields is correct.
+    if (validation.error) {
+        res.status(400).send(validation.error.details[0].message);
+    } else {
+        // try to find the user in the database, if they do not exist, exit
+        const userLogin = await User.findOne({
+            displayName: req.body.displayName.trim(),
+        });
+        if (userLogin) {
+            // if the user does exist in the db, check whether the password is correct, if not send to catch.
+            const loggedIn = bcrypt.compareSync(
+                req.body.password,
+                userLogin.password
+            );
+            if (loggedIn) {
+                // the user is logged in
+                res.status(200).send("Logged In");
+            } else {
+                res.status(400).send(
+                    "The user/password combination is incorrect."
+                );
+            }
+        } else {
+            res.status(400).send("The user/password combination is incorrect.");
+        }
+    }
 });
 
 module.exports = router;
