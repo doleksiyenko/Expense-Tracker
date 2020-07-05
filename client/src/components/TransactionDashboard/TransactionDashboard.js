f72ddfc1027057510644145d2c63ea71cfa92724
doleksiyenko <dan.oleksiyenko@gmail.com>
2020-07-05 18:39:09 -0400

    User transactions load onto screen (p tag)

--- client/src/components/TransactionDashboard/TransactionDashboard.js
+++ client/src/components/TransactionDashboard/TransactionDashboard.js
@@ -1,12 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { TransactionContext } from "../../context/TransactionContext";
 
 const TransactionDashboard = () => {
     // reducer for the user's transactions
-    const [transactionState, transactionDispatch] = useContext(
-        TransactionContext
+    const [transaction, setTransactions] = useContext(TransactionContext);
+    useEffect(() => {
+        const getTransactions = async () => {
+            // get the token from local storage
+            let token = localStorage.getItem("auth-token");
+            // try to fetch the transactions from the server
+            const response = await fetch(
+                "http://localhost:5000/api/transactions",
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "auth-token": token,
+                    },
+                    method: "POST",
+                }
+            );
+            const transactions = await response.json();
+            if (response.status === 200) {
+                setTransactions(transactions);
+            } else {
+                return setTransactions([transactions.error]);
+            }
+        };
+        getTransactions();
+    }, []);
+
+    return (
+        <div>
+            <h1>TransactionDashboard</h1>
+            {transaction.map((transaction) => (
+                <p key={transaction._id}>
+                    {transaction.expenseMessage}, {transaction.expense}
+                </p>
+            ))}
+        </div>
     );
-    return <h1>TransactionDashboard</h1>;
 };
 
 export default TransactionDashboard;
--- client/src/context/TransactionContext.js
+++ client/src/context/TransactionContext.js
@@ -1,30 +1,13 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useState } from "react";
 
 // create a context for providing user transactions
 export const TransactionContext = createContext();
 
-const initialState = { transactions: [] };
-
-// A reducer for retrieving, creating and deleting transactions from the server
-const transactionReducer = (state, action) => {
-    switch (action.type) {
-        case "GET_TRANSACTIONS":
-            return { transactions: ["GET TRANSACTION"] };
-        default:
-            throw new Error();
-    }
-};
-
-// provide the useReducer context to children
+// provide the transaction state context to children
 export const TransactionProvider = ({ children }) => {
-    const [transactionState, transactionDispatch] = useReducer(
-        transactionReducer,
-        initialState
-    );
+    const [transaction, setTransactions] = useState([]);
     return (
-        <TransactionContext.Provider
-            value={[transactionState, transactionDispatch]}
-        >
+        <TransactionContext.Provider value={[transaction, setTransactions]}>
             {children}
         </TransactionContext.Provider>
     );
